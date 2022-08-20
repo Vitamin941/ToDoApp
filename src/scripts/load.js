a2f8af0fe8c2cb93338740faadfe075ba3704150
Vitaliy Biryuckov <stukov.biryuckov2017@yandex.ru>
2022-08-21 00:03:59 +0800

    create a todo and create-todo function

--- src/scripts/load.js
+++ src/scripts/load.js
@@ -1,23 +1,16 @@
-const currMonthName = (date) => {
-    let month = date + 1
-    switch (month) {
-        case 1: return "January"
-        case 2: return "February"
-        case 3: return "March"
-        case 4: return "April"
-        case 5: return "May"
-        case 6: return "Junee"
-        case 7: return "July"
-        case 8: return "August"
-        case 9: return "September"
-        case 10: return "October"
-        case 11: return "November"
-        case 12: return "Deсember"
-    }
+var H1title = document.getElementsByClassName("title")[0]
+var container = document.getElementsByClassName("card-container")[0]
+H1title.innerText = new Date().toLocaleDateString()
+
+
+const createTodo = () => {
+    let div = document.createElement("div")
+    div.classList += "flex flex-row min-w-fit w-40 h-10 border-2 border-solid border-neutral-400 mt-4"
+    div.innerHTML += `
+    <input class="text-sm lg:text-lg text-center h-full" placeholder="...">
+    <div class="flex items-center justify-center h-full border-l-2 border-solid border-red-600">
+        <input class="focus:outline-hidden w-8 h-4" type="checkbox" name="check">
+        `
+    container.appendChild(div)
+    console.log("clicked")
 }
-
-
-let H1title = document.getElementsByClassName("title")[0]
-let currentDate = new Date()
-// H1title.innerText = currentDate.getDate() + " " + currMonthName(currentDate.getMonth()) + " " + currentDate.getFullYear()
-
