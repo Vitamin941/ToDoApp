97ab812b3dc32964c031591c4c0e8445a9684967
Vitaliy Biryuckov <stukov.biryuckov2017@yandex.ru>
2022-08-23 22:22:12 +0800

    Create a toggle todo method

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -7,11 +7,25 @@ const createTodo = () => {
     let div = document.createElement("div")
     div.classList += "flex flex-row min-w-fit w-40 h-10 border-2 border-solid border-neutral-400 mt-4"
     div.innerHTML += `
-    <input class="text-sm lg:text-lg text-center h-full" placeholder="...">
+    <input class="focus:outline-none text-sm lg:text-lg text-center h-full text-container" placeholder="...">
     <div class="flex items-center justify-center h-full border-l-2 border-solid border-red-600">
-        <input class="focus:outline-hidden w-8 h-4" type="checkbox" name="check">
-        `
+        <input class="w-8 h-4 checkbox" type="checkbox" name="check" onchange="toggleTodo()">
+    `
     container.appendChild(div)
-    console.log("clicked")
 }
 
+
+const toggleTodo = () => {
+    let checkBoxTodo = document.querySelectorAll(".checkbox")
+    let textContainers = document.querySelectorAll(".text-container")
+    for (let i = 0; i < checkBoxTodo.length; i++) {
+        if (checkBoxTodo[i].checked) {
+            checkBoxTodo[i].parentElement.classList.add("opacity-10")
+            textContainers[i].classList.add("line-through")
+        }
+        else {
+            checkBoxTodo[i].parentElement.classList.remove("opacity-10")
+            textContainers[i].classList.remove("line-through")
+        }  
+    }
+}
